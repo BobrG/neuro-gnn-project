1c88a44e2a1e41c32756f786ab8e2dfec16cad80
BobrG <bobrovskihg@gmail.com>
2022-10-26 10:13:23 +0000

    [wip]

---
+++ install_req.sh
@@ -0,0 +1,4 @@
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch-geometric -f https://data.pyg.org/whl/torch-1.12.1+cu116.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.12.1+cu116.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.12.1+cu116.html
